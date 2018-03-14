bac705a75909376ba840a6de36e7c50f47717fef
rafael moreira <rafael@rafaelmoreira.org>
2018-03-13 23:56:40 -0300

    add ace window for window management

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-bullets which-key try use-package))))
+ '(package-selected-packages (quote (ace-window org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -47,3 +47,13 @@
 (ido-mode 1)
 
 (defalias 'list-buffers 'ibuffer)
+
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+    ))
