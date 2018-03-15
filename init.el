a9c2bef56f112d0f1ae14397f74d9d4be7c74ac9
rafael moreira <rafael@rafaelmoreira.org>
2018-03-15 01:58:54 -0300

    bunch of stuff

--- init.el
+++ init.el
@@ -1,17 +1,18 @@
-;; auto added stuff
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ace-window org-bullets which-key try use-package))))
+ '(package-selected-packages
+   (quote
+    (counsel-projectile counsel ivy projectile molokai-theme color-theme-modern auto-complete ace-window org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
 
 (setq inhibit-startup-message t) ;; do not exibit the inicial message
 (tool-bar-mode -1)
@@ -41,7 +42,7 @@
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 
-;; configs
+;; ido configs
 (setq indo-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
@@ -57,3 +58,66 @@
      '(aw-leading-char-face
        ((t (:inherit ace-jump-face-foreground :height 3.0)))))
     ))
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
+(use-package color-theme-modern
+  :ensure t)
+
+(use-package molokai-theme
+  :ensure t
+  :config (load-theme 'molokai t))
+
+(use-package projectile
+  :ensure t
+  :config (projectile-mode))
+
+(use-package ivy
+	     :ensure t)
+(use-package counsel
+	     :ensure t)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+(use-package counsel-projectile
+  :ensure t
+  :config (counsel-projectile-mode))
+
+;(use-package highlight-indentation
+;  :ensure t
+;  :config
+;  (progn
+;    (highlight-indentation-mode)
+;    (highlight-indentation-current-column-mode)
+;    (set-face-background 'highlight-indentation-face "#ffffff")
+;    (set-face-background 'highlight-indentation-current-column-face "#CCCCCC")))
+
+(global-hl-line-mode t) ;; hightlight the current line
+(electric-pair-mode 1) ;; auto add matching pair
+(show-paren-mode 1) ;; hightlight matching parens
+(global-linum-mode 1) ;; show line numbers
+(column-number-mode 1) ;; show column number
+(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
