fefce33d8828fbb003b2cefff15a87b5932720fa
Rafael Moreira <rafael@rafaelmoreira.org>
2018-12-22 03:05:34 -0200

    rebuilding emacs"

--- init.el
+++ init.el
@@ -1,130 +1,66 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (alchemist counsel-projectile counsel ivy projectile molokai-theme color-theme-modern auto-complete ace-window org-bullets which-key try use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
+(straight-use-package 'use-package)
 
-(setq inhibit-startup-message t) ;; do not exibit the inicial message
-(tool-bar-mode -1)
-
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(use-package try
-  :ensure t)
-
+;;; which-key
 (use-package which-key
-  :ensure t
+  :straight t
   :config (which-key-mode))
 
-;; Org-mode stuff
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+;;; org mode
 
+(require 'subr-x)
+(straight-use-package 'git)
+
+(defun org-git-version ()
+  "The Git version of org-mode.
+Inserted by installing org-mode or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (git-run "describe"
+              "--match=release\*"
+              "--abbrev=6"
+              "HEAD"))))
+
+(defun org-release ()
+  "The release version of org-mode.
+Inserted by installing org-mode or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (string-remove-prefix
+      "release_"
+      (git-run "describe"
+               "--match=release\*"
+               "--abbrev=0"
+               "HEAD")))))
+
+(provide 'org-version)
+
+(straight-use-package 'org-plus-contrib) ; or org-plus-contrib if desired
 
 ;; ido configs
 (setq indo-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
-(defalias 'list-buffers 'ibuffer)
+(defalias 'list-buffers 'ibuffer) ;;; i don't know what this does
 
 (use-package ace-window
-  :ensure t
-  :init
-  (progn
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-    ))
+  :straight t)
 
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-(use-package color-theme-modern
-  :ensure t)
-
-(use-package molokai-theme
-  :ensure t
-  :config (load-theme 'molokai t))
-
-(use-package projectile
-  :ensure t
-  :config (projectile-mode))
-
-(use-package ivy
-	     :ensure t)
-(use-package counsel
-	     :ensure t)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
-(use-package counsel-projectile
-  :ensure t
-  :config (counsel-projectile-mode))
-
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
-;;; elixir
-
-(use-package alchemist
-  :ensure t)
-
-;(use-package highlight-indentation
-;  :ensure t
-;  :config
-;  (progn
-;    (highlight-indentation-mode)
-;    (highlight-indentation-current-column-mode)
-;    (set-face-background 'highlight-indentation-face "#ffffff")
-;    (set-face-background 'highlight-indentation-current-column-face "#CCCCCC")))
-
-(global-hl-line-mode t) ;; hightlight the current line
-(electric-pair-mode 1) ;; auto add matching pair
-(show-paren-mode 1) ;; hightlight matching parens
-(global-linum-mode 1) ;; show line numbers
-(column-number-mode 1) ;; show column number
-(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
+(global-set-key (kbd "C-x o") 'ace-window)
