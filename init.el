9e3b46622e6e761b32e20496df5dd06751495fc8
Rafael Moreira <rafael@rafaelmoreira.org>
2018-12-28 07:27:06 -0200

    add old stuff again

--- init.el
+++ init.el
@@ -64,3 +64,89 @@ Inserted by installing org-mode or when a release is made."
   :straight t)
 
 (global-set-key (kbd "C-x o") 'ace-window)
+
+(use-package auto-complete
+  :straight t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
+(use-package color-theme-modern
+  :straight t)
+
+(use-package molokai-theme
+  :straight t
+  :config (load-theme 'molokai t))
+
+(use-package projectile
+  :straight t
+  :config (projectile-mode +1))
+
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(use-package ivy
+  :straight t)
+
+(use-package counsel
+  :straight t)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+(use-package counsel-projectile
+  :straight t
+  :config (counsel-projectile-mode))
+
+;(use-package highlight-indentation
+;  :ensure t
+;  :config
+;  (progn
+;    (highlight-indentation-mode)
+;    (highlight-indentation-current-column-mode)
+;    (set-face-background 'highlight-indentation-face "#ffffff")
+;    (set-face-background 'highlight-indentation-current-column-face "#CCCCCC")))
+
+(global-hl-line-mode t) ;; hightlight the current line
+(electric-pair-mode 1) ;; auto add matching pair
+(show-paren-mode 1) ;; hightlight matching parens
+(global-linum-mode 1) ;; show line numbers
+(column-number-mode 1) ;; show column number
+(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
+
+
+;;;; ruby
+
+(use-package enh-ruby-mode
+  :straight t)
+
+(add-to-list 'auto-mode-alist
+             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+
+(setq enh-ruby-add-encoding-comment-on-save nil) ;;; disable auto insert of magic comments on save
+
+
+(use-package rinari
+  :straight t)
+
+(use-package ruby-end
+  :straight t)
